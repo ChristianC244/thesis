89f19d03b52584c2d072a515c6e00956bdb3305d
ChristianC244 <christiancattai@gmail.com>
2022-07-07 16:51:40 +0200

    Added postitional args for threads and max execution time

--- script.py
+++ script.py
@@ -18,20 +18,27 @@ from lib.Manager import Manager
 
 from dotenv import load_dotenv
 import os
+import argparse
 
+# Parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--threads", type=int, help="Set how many threads", default=8)
+parser.add_argument("-e", "--execution", type=int, help="Maximum execution time in MINUTES for each analysis", default=30)
+args = parser.parse_args()
+# Load dir
 wd = os.path.dirname(os.path.realpath(__file__))+"/"
 load_dotenv()
 # INFURA_ID = os.getenv("INFURA_ID")
 API_KEY = os.getenv("API_KEY")
-THREADS = 8
-TIME = 60 * 30 # 30 minutes
+threads = args.threads
+time = 60 * args.execution 
 
 def main():
-    try:    
-        man = Manager(API_KEY, THREADS, TIME)
+    try:
+        man = Manager(API_KEY, threads, time)
         man.start_scan()
     except KeyboardInterrupt:
-        print(f"\nGracefully stopping...\n[it can take up to {TIME//60} min]\nPress again Ctrl-C to hard stop!")
+        print(f"\nGracefully stopping...\n[it can take up to {time//60} min]\nPress again Ctrl-C to hard stop!")
         man.stopped = True
 
 if __name__ == "__main__":
